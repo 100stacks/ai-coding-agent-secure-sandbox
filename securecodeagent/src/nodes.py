eedd4f057b29a6d99ec57dc7db0ab51aff5e3890
100stacks <100stacks@users.noreply.github.com>
2025-05-11 20:15:44 -0400

    agent: build code prompt

--- securecodeagent/src/nodes.py
+++ securecodeagent/src/nodes.py
@@ -78,3 +78,53 @@ class Nodes:
 
         # Parser
         parser_tool = PydanticToolsParser(tools=[Code])
+
+        ## Prompt
+        template = """
+You are a coding assistant with expertise in Python.
+You are able to execute Python code in a secure sandbox environment.
+You are tasked with responding to the following user question: {question}
+Your response will be shown to the user.
+Here is a full set of documentation:
+
+---------
+{context}
+---------
+
+Answer the user question based on the above provided documentation.
+Ensure any code you provide can be executed with all required imports and variables defined.
+Structure your answer as a description fo the code solution,
+then a list of the imports, and then finally list the functioning code block.
+Here is the user question again:
+
+--- --- ---
+{question}
+"""
+
+    ## Generation
+    if "error" in state_dict:
+        print("---RE-GENERATE SOLUTION with ERROR FEEDBACK---")
+
+        error = state_dict["error"]
+        code_solution = state_dict["generation"]
+
+        # Update prompt
+        addendum = """You previously tried to solve this problem.  Here is your solution:
+
+{generation}
+
+Here is the resulting error from code execution:
+
+{error}
+
+Please retry to answer this.  Structure your answer with a description of the code solution.
+Then list the imports. And finally list the functioning code block.  Structure your answer
+with a description of the code solution.  Then list the imports. And finally list the
+functioning code block.
+
+Here is the user question:
+
+{question}
+"""
+        ## Prompt
+        template = template + addendum
